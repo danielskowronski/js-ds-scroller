b3bccfe53d83599f627831bdfaf40d9b4acb7ed9
Daniel Skowroński <daniel@dsinf.net>
2015-09-28 20:40:08 +0200

    event changed
    event changed to handle only mouse scroll, not any page scroll (incl. link hash jumping to page section)

--- scroller.js
+++ scroller.js
@@ -7,30 +7,28 @@ var currentElement = baseOfElements.first();
 
 function parseScroll() { 
   clearTimeout( $.data( this, "scrollCheck" ) );
-  $.data( this, "scrollCheck", setTimeout(function() {
+    $.data( this, "scrollCheck", setTimeout(function() {
     var st = $(this).scrollTop();
-
     if (st > lastScrollTop){
         if (! currentElement.is(baseOfElements.last()))
-          currentElement = currentElement.next();
+        currentElement = currentElement.next();
     } else if (st < lastScrollTop){
         if (! currentElement.is(baseOfElements.first()))
-          currentElement = currentElement.prev();
+        currentElement = currentElement.prev();
     }
 
-    $(window).unbind("scroll", parseScroll);
-    $('html, body').animate(
-      {
+    $(document).unbind("DOMMouseScroll", parseScroll);
+    $('html, body').animate({
         scrollTop: $(currentElement).offset().top - 50
-      },
-      {
+        },
+        {
         duration: 150,
         complete: function(){ 
-          setTimeout( function(){$(window).bind("scroll", parseScroll); lastScrollTop=$(this).scrollTop(); }, 10 );
+            setTimeout( function(){$(document).bind("DOMMouseScroll", parseScroll); lastScrollTop=$(this).scrollTop(); }, 10 );
+          }
         }
-      }
     );
   }, 250));
 }
 
-$(window).bind("scroll", parseScroll);
+$(document).bind("DOMMouseScroll", parseScroll);
